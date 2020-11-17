a3753d2aced1ebd8072c0cd05592ac8ddca5a717
red-panda-web <readex934@gmail.com>
2020-11-17 20:53:37 +0300

    complete menu and underheader slider

--- script.js
+++ script.js
@@ -2,4 +2,19 @@
 
 document.addEventListener("DOMContentLoaded", function () {
 
+	new Swiper('.under-header__slider', {
+		direction: 'horizontal',
+		loop: true,
+		slidesPerView: 1,
+		slideClass: 'under-header__slide',
+		wrapperClass: 'under-header__slider-wrap',
+
+		pagination: {
+			el: '.under-header__pagination',
+			clickable: true,
+		},
+		autoplay: {
+			delay: 5000,
+		},
+	})
 });
